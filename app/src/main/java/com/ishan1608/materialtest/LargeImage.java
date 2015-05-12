53f4635859023222accfca75ca7b016b9680df1f
Ishan <ishanatmuzaffarpur@gmail.com>
2015-05-13 02:34:52 +0530

    Unfinished second activity

---
+++ app/src/main/java/com/ishan1608/materialtest/LargeImage.java
@@ -0,0 +1,15 @@
+package com.ishan1608.materialtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class LargeImage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_large_image);
+    }
+
+}
--- app/src/main/java/com/ishan1608/materialtest/MainActivity.java
+++ app/src/main/java/com/ishan1608/materialtest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ishan1608.materialtest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
@@ -10,6 +13,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Getting handle
+        ImageView smallImageView1 = (ImageView) findViewById(R.id.small_imageView_1);
+        if (smallImageView1 != null) {
+            smallImageView1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent largeImagesIntent = new Intent(MainActivity.this, LargeImage.class);
+                    startActivity(largeImagesIntent);
+                }
+            });
+        }
     }
 
 }
